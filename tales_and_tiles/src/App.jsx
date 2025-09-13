c158c7e135fe0b1795a4108ef055df63a37bcdf0
codebyprins <astridprins2@gmail.com>
2025-09-14 01:18:45 +0200

    project setup + documentation

---
+++ tales_and_tiles/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        Ready... Set... Start!
+        <br></br>
+        Welcome to my first attempt on my new Webbased RPG
+        <br></br>
+        We'll see where it goes :)
+      </div>
+    </>
+  )
+}
+
+export default App
