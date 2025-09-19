54b9f9ab583e3e2d01c4e54d82bf958f7cc55b75
codebyprins <astridprins2@gmail.com>
2025-09-19 19:53:39 +0200

    project setup + documentation 2.0

--- tales_and_tiles/src/App.jsx
+++ tales_and_tiles/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import logo from './assets/tales_and_tiles_64x64.png'
 
 function App() {
   return (
     <>
       <div>
-        Ready... Set... Start!
+        <img src={logo}></img>
         <br></br>
+        <h1>Tales and Tiles</h1>
+        <h2>Ready... Set... Start!</h2>
         Welcome to my first attempt on my new Webbased RPG
         <br></br>
         We'll see where it goes :)
